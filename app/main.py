6cd6f0e773843156b1dac3395695e874747ba9c2
PN-95 <68422890+PN-95@users.noreply.github.com>
2025-09-27 05:34:26 +0200

    add CORS support

--- app/main.py
+++ app/main.py
@@ -1,7 +1,21 @@
 # app/main.py
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# Allow requests from your frontend
+origins = [
+    "http://localhost:8080",  # your frontend
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # or ["*"] to allow all
+    allow_credentials=True,
+    allow_methods=["*"],  # GET, POST, etc.
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
